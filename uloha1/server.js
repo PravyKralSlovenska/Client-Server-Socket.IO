a55b228f24ccefdc1168ff884cf47a3241891694
PravyKralSlovenska <velkaricka12@gmail.com>
2024-06-02 21:18:02 +0200

    nech mi boh odpusti

--- uloha1/server.js
+++ uloha1/server.js
@@ -1,5 +1,16 @@
-const io = require('socket.io')(3000);
+const http = require("http").createServer();
+const io = require('socket.io')(http, {
+    cors: {origin: "*"}
+});
 
 io.on("connection", (socket) => {
-    console.log("Vitajte kokoti")
+    console.log("kokot sa pripojil");
+
+    socket.on("message", (message) => {
+        console.log(message);
+        on.emit("message", `typek povedal: ${message}`)
+    });
 });
+
+const port = 3000;
+http.listen(port, () => console.log(`Server je zapnuty, pocuvam na porte ${port}`));
