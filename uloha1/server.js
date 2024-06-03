39aeca8a8a36cae7470e043f292cfdae9fbaa7bc
PravyKralSlovenska <stefancikjakub1@gmail.com>
2024-06-03 22:01:45 +0200

    vsetko funguje

--- uloha1/server.js
+++ uloha1/server.js
@@ -2,13 +2,51 @@ const http = require("http").createServer();
 const io = require('socket.io')(http, {
     cors: {origin: "*",}
 });
+// !!!!!!
+// ked je v miestnosti 0 ludi, tak server spadne (nie je osetrene, nechce sa mi osetrit)
+const miestnosti = []; // [["default", [[anonym1, farba1]]], ["room1", [peter, farba2]]]
+var n = 0;
 
 io.on("connection", (socket) => {
-    console.log("kokot sa pripojil");
+    console.log("typek sa pripojil");
+
+    socket.on("init", (meno, miestnost) => {
+        if (meno === "") {
+            meno = `anonym_${n++}`;
+        }
+        
+        socket.meno = meno;
+        socket.miestnost = miestnost;
+        socket.farba = ranFarba();
+        
+        if (miestnosti.find((miestnost) => miestnost[0] === socket.miestnost)) {
+            miestnosti.find((miestnost) => miestnost[0] === socket.miestnost)[1].push([socket.meno, socket.farba]);
+        } else {
+            miestnosti.push([socket.miestnost, [[socket.meno, socket.farba]]]);
+        }
+        
+        socket.join(socket.miestnost);
+
+        io.to(socket.miestnost).emit("message", `<b>${nowTime()} Server: <span style="color: ${socket.farba}">${socket.meno}</span></b> sa pripojil do miestnosti ${socket.miestnost}`);
+        io.to(socket.miestnost).emit("update_list", miestnosti.find((miestnost) => miestnost[0] === socket.miestnost)[1]);
+    });
 
     socket.on("message", (message) => {
-        console.log(message);
-        io.emit("message", `typek povedal: ${message}`)
+        io.to(socket.miestnost).emit("message", `<b>${nowTime()} <span style="color: ${socket.farba}">${socket.meno}</span>:</b> ${message}`);
+    });
+
+    socket.on("disconnect", () => {
+        io.to(socket.miestnost).emit("message", `<b>${nowTime()} Server: <span style="color: ${socket.farba}">${socket.meno}</span></b> sa odpojil z chatu`);
+        
+        // if (miestnosti.find((miestnost) => miestnost[0] === socket.miestnost)[1].length === 1) {} // neviem ci 1 alebo 0 :()
+        try {
+            miestnosti.find((miestnost) => miestnost[0] === socket.miestnost)[1] = miestnosti.find((miestnost) => miestnost[0] === socket.miestnost)[1].filter((clovek) => clovek[0] !== socket.meno);
+            io.to(socket.miestnost).emit("update_list", miestnosti.find((miestnost) => miestnost[0] === socket.miestnost)[1]);
+        } catch (error) {
+            console.log(error)
+        }
+
+        console.log("typek sa odpojil");
     });
 });
 
@@ -17,3 +55,13 @@ http.listen(port, () => console.log(`Server je zapnuty, pocuvam na porte ${port}
 
 // http://127.0.0.1:3000/
 // http://localhost:3000/methods: ["GET", "POST"]
+
+function ranFarba() {
+    let zoznam = ["red", "blue", "green", "purple", "orange", "brown", "black"];
+    return zoznam[Math.floor(Math.random() * zoznam.length)];
+}
+
+function nowTime() {
+    let date = new Date();
+    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
--- uloha1/user.js
+++ uloha1/user.js
@@ -4,9 +4,9 @@ const socket = io(`http://localhost:${port2}`);
 const spravy = document.getElementById("spravy");
 const input = document.getElementById("input");
 const tlacidlo = document.getElementById("tlacidlo");
-
-// const list = document.getElementById("list");
-// list.appendChild(document.createElement("li")).innerHTML = "typek2";
+const list_el = document.getElementById("list");
+const miestnost_el = document.getElementById("miestnost");
+const nadpis = document.getElementById("nadpis");
 
 socket.on("message", (text) => {
     const el = document.createElement("li");
@@ -14,9 +14,45 @@ socket.on("message", (text) => {
     spravy.appendChild(el)
 });
 
+socket.on("update_list", (list) => {
+    document.getElementById("pripojeni").innerHTML = list.length;
+    list_el.innerHTML = "";
+
+    list.forEach((clovek) => {
+        console.log(clovek)
+        let li = document.createElement("li");
+        li.innerHTML = `<b><span style='color: ${clovek[1]};'>${clovek[0]}</span></b>`;
+        list_el.appendChild(li);
+    });
+});
+
 tlacidlo.addEventListener("click", (event) => {
     event.preventDefault();
-    console.log("klikol si na tlacidlo");
     socket.emit("message", input.value);
     input.value = "";
+});
+
+input.addEventListener("keyup", (event) => {
+    console.log("key pressed")
+    if (event.key === "Enter") {
+        event.preventDefault();
+        tlacidlo.click();
+    }
+});
+
+document.getElementById("pripojitButton").addEventListener("click", (event) => {
+    event.preventDefault();
+    let meno = document.getElementById("menoInput").value;
+    let miestnost = document.getElementById("miestnostInput").value;
+
+    if (miestnost === "") {
+        miestnost = "default";
+    }
+
+    socket.emit("init", meno, miestnost);
+
+    miestnost_el.innerHTML = miestnost;
+    document.getElementById("menicko").style.display = "none";
+    document.getElementById("bodicko").style.visibility = "visible";
+    document.getElementById("meno").innerHTML = meno;
 });
